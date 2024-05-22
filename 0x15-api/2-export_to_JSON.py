4b63189e3704a1504a8d61480e004d8afc23d0db
DORCAS AUMA <dorcasauma89@gmail.com>
2024-05-22 14:24:20 +0300

    json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 
-
 """
 This script fetches tasks for a specified user ID from the JSONPlaceholder API
-and exports the data to a CSV file.
+and exports the data to a JSON file.
 """
 
-
-import csv
-import requests
+import json
 import sys
+import requests
 
-
-def export_to_csv(user_id, username, tasks):
+def export_to_json(user_id, username, tasks):
     """
-    Export tasks data to a CSV file.
+    Export tasks data to a JSON file.
 
     Args:
         user_id (int): The user ID.
@@ -24,19 +21,10 @@ def export_to_csv(user_id, username, tasks):
     Returns:
         None
     """
-    filename = f"{user_id}.csv"
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = ['USER_ID', 'USERNAME',
-                      'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        for task in tasks:
-            writer.writerow({
-                'USER_ID': user_id,
-                'USERNAME': username,
-                'TASK_COMPLETED_STATUS': task.get('completed'),
-                'TASK_TITLE': task.get('title')
-            })
-
+    data = {str(user_id): [{"task": task.get('title'), "completed": task.get('completed'), "username": username} for task in tasks]}
+    filename = f"{user_id}.json"
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=2)
 
 def fetch_tasks(user_id):
     """
@@ -53,12 +41,10 @@ def fetch_tasks(user_id):
     if response.status_code == 200:
         return response.json()
 
-
 if __name__ == "__main__":
-
     user_id = int(sys.argv[1])
     tasks = fetch_tasks(user_id)
     if tasks:
         # Assuming the username is the same for all tasks
         username = tasks[0].get('username')
-        export_to_csv(user_id, username, tasks)
+        export_to_json(user_id, username, tasks)
